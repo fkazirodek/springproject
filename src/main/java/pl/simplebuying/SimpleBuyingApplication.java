7445c32deea1c317c141e3cac8fb6cbcc6a79d9b
Filip K <filipk@gmail.com>
2017-07-18 14:52:02 +0200

    Initial commit

---
+++ src/main/java/pl/simplebuying/SimpleBuyingApplication.java
@@ -0,0 +1,12 @@
+package pl.simplebuying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBuyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBuyingApplication.class, args);
+	}
+}
